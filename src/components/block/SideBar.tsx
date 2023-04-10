f06a49f3dce8239a6f68b1d12cfad0517042430d
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-10 14:20:59 +0100

    Sidebar

--- src/components/block/SideBar.tsx
+++ src/components/block/SideBar.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <Container>
+
+    </Container>
   )
 }
 
 export default SideBar
+const Container = styled.div`
+  width: 270px;
+  height: 100vh;
+  position: fixed;
+  background-color: #f2f5f8;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
+  @media screen and (max-width: 1024px) {
+    display: none;
+  }
+`
