61932ff3e059abe6d82d738108e676c829da70e8
Dc-Valerian <valerianpedro03@gmail.com>
2023-04-11 20:33:27 +0100

    added new file

--- src/pages/landingpage/HomeScreen.tsx
+++ src/pages/landingpage/HomeScreen.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
 import Home from "./Home"
 import Connect from "./Connect"
 import Plan from './Plan'
+import Payment from "./Payment"
+
 const HomeScreen = () => {
   return (
     <div>
         <Home/>
         <Connect/>
         <Plan/>
+        <Payment/>
     </div>
   )
 }
