5aa33362b6c75c2412007fa9dec95f46a10c8a22
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-12 11:36:30 +0100

    payroll search

--- src/pages/Payroll.tsx
+++ src/pages/Payroll.tsx
@@ -14,12 +14,84 @@ const Payroll = () => {
                       <Input placeholder='Search people'/>
                   </Inputhold>
               </Head>
+
+              <Table>
+                  <Lists>
+                      <Name>Full Name</Name>
+                      <Gross>Gross pay</Gross>
+                      <Taxes>Est. Employee Taxes</Taxes>
+                      <Netpay>Est. Net pay</Netpay>
+                      <Payment>Payment method</Payment>
+                      <Status>Status</Status>
+                  </Lists>
+              </Table>
           </Wrapper>
       </Container>
   )
 }
 
 export default Payroll
+const Status = styled.div`
+    width: 120px;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    font-size: 13px;
+`
+const Payment = styled.div`
+    width: 180px;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    font-size: 13px;
+`
+
+const Netpay = styled.div`
+    width: 120px;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    font-size: 13px;
+`
+
+const Taxes = styled.div`
+    width: 180px;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    font-size: 13px;
+`
+
+const Gross = styled.div`
+    width: 120px;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    font-size: 13px;
+`
+
+const Name = styled.div`
+    width: 250px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+`
+
+const Lists = styled.div`
+    width: 95%;
+    height: 100%;
+    display: flex;
+`
+
+const Table = styled.div`
+    width: 100%;
+    height: 50px;
+    border-bottom: 1px solid #c5c5c5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const Input = styled.input`
     height: 100%;
@@ -51,22 +123,21 @@ const Inputhold = styled.div`
 const Head = styled.div`
     width: 100%;
     height: 55px;
-    border-bottom: 1px solid #eee;
+    border-bottom: 1px solid #d3d3d3;
     display: flex;
     align-items: center;
 `
 
 const Wrapper = styled.div`
     width: 93%;
-    height: 500px;
     display: flex;
     border: 1px solid #d6dee9;
     margin-top: 110px;
+    flex-direction: column;
 `
 
 const Container = styled.div`
     width: calc(100vw - 270px);
     display: flex;
-    height: 400px;
     justify-content: center;
 `
